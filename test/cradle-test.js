9fe9a20f6704b36358cf8143323279a895a3971f
cloudhead <self@cloudhead.net>
2010-06-09 16:13:41 -0400

    move db preparation to scripts/ folder

--- test/cradle-test.js
+++ test/cradle-test.js
@@ -5,10 +5,9 @@ var path = require('path'),
     http = require('http'),
     fs = require('fs');
 
-require.paths.unshift(path.join(__dirname, '..', 'lib'));
+require('./scripts/prepare-db');
 
-var vows = require('vows'),
-    cradle = require('cradle');
+require.paths.unshift(path.join(__dirname, '..', 'lib'));
 
 function status(code) {
     return function (res) {
@@ -25,48 +24,8 @@ function mixin(target) {
     return target;
 }
 
-var client = http.createClient(5984, '127.0.0.1');
-
-function r(method, url, doc) {
-    var promise = new(events.EventEmitter);
-    var request = client.request(method, url, {});
-
-    if (doc) { request.write(JSON.stringify(doc)) }
-
-    request.addListener('response', function (res) {
-        var body = '';
-
-        res.setEncoding('utf8');
-        res.addListener('data', function (chunk) {
-            body += (chunk || '');
-        }).addListener('end', function () {
-            var obj, response;
-
-            try { obj = JSON.parse(body) }
-            catch (e) { return promise.emit('error', e) }
-
-            promise.emit('success', obj);
-        });
-    });
-    request.end();
-    return promise;
-}
-
-['rabbits', 'pigs','badgers'].forEach(function (db) {
-    r('DELETE', '/' + db);
-});
-r('PUT', '/rabbits');
-r('PUT', '/pigs');
-r('PUT', '/pigs/_design/pigs', {
-    _id: '_design/pigs', views: {
-        all: { map: "function (doc) { if (doc.color) emit(doc._id, doc) }" }
-    }
-});
-r('PUT', '/pigs/mike', {color: 'pink'});
-r('PUT', '/rabbits/alex', {color: 'blue'});
-r('PUT', '/pigs/bill', {color: 'blue'});
-
-process.loop();
+var cradle = require('cradle');
+var vows = require('vows');
 
 vows.describe("Cradle").addVows({
     "Default connection settings": {
